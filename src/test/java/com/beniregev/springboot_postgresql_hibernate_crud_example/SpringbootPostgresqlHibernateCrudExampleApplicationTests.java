425e1168093d2370946d7b55b24b477c165cff22
binyamin.regev <beniregev@gmail.com>
2022-03-04 11:27:32 +0200

    Adding SpringBootApplication and SpringBootApplicationTest classes to project

---
+++ src/test/java/com/beniregev/springboot_postgresql_hibernate_crud_example/SpringbootPostgresqlHibernateCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beniregev.springboot_postgresql_hibernate_crud_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgresqlHibernateCrudExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
